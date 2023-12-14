b6703db77cc144dc0f362bce31161c9c51083308
Merajshaikh89 <merajshaikh.ms@gmail.com>
2023-12-14 15:57:43 -0500

    2.9

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ const { MongoClient } = require("mongodb");
 const { log } = require("console");
 const passport = require("passport");
 let pass = require("./passport.js");
+const { check, validationResult } = require("express-validator");
 
 // Initialize Express app
 const app = express();
+const cors = require("cors");
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 let auth = require("./auth.js")(app);
 const port = 8080;
 app.use(express.json());
@@ -170,32 +189,53 @@ app.get(
 //--------- POST Requests -------------
 
 // Route to add a new user
-app.post("/users", async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + "already exists");
-      } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-        })
-          .then((user) => {
-            res.status(201).json(user);
+app.post(
+  "/users",
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
+  async (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+      .then((user) => {
+        if (user) {
+          //If the user is found, send a response that it already exists
+          return res.status(400).send(req.body.Username + " already exists");
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
           })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-          });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    });
-});
+            .then((user) => {
+              res.status(201).json(user);
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send("Error: " + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 // Add a movie to a user's list of favorites
 app.post(
   "/users/:Username/movies/:MovieID",
@@ -227,6 +267,7 @@ app.post(
 );
 
 //----------- PUT Requests ---------
+//Update user
 app.put(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
@@ -269,7 +310,7 @@ app.delete(
     }
 
     try {
-      const user = await Users.findOneAndRemove({
+      const user = await Users.findOneAndDelete({
         Username: req.params.Username,
       });
 
